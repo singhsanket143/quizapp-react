619a4654fb07c3b6bb0043594c4b16988fb8f3af
Sourav Sinha <4souravsinhain@gmail.com>
2024-01-28 16:14:46 +0530

    update score and color

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,67 @@
 /* eslint-disable react/jsx-key */
-import { useState } from 'react';
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   console.log("Rendered");
-
+  const [optionSelected, setOptionSelected] = useState(0);
   let [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
 
+  function updateScore() {
+    setScore(score + 1);
+  }
   const quizQuestions = [
     {
       question: "What is the complexity of binary search ?",
       options: [
-        { answer: 'O(n)', isCorrect: false},
-        { answer: 'O(logn)', isCorrect: true},
-        { answer: 'O(nlogn)', isCorrect: false},
-        { answer: 'O(1)', isCorrect: false}
-      ]
+        { answer: "O(n)", isCorrect: false },
+        { answer: "O(logn)", isCorrect: true },
+        { answer: "O(nlogn)", isCorrect: false },
+        { answer: "O(1)", isCorrect: false },
+      ],
     },
     {
       question: "What is the complexity of linear search ?",
       options: [
-        { answer: 'O(n)', isCorrect: true},
-        { answer: 'O(logn)', isCorrect: false},
-        { answer: 'O(nlog)', isCorrect: false},
-        { answer: 'O(1)', isCorrect: false}
-      ]
+        { answer: "O(n)", isCorrect: true },
+        { answer: "O(logn)", isCorrect: false },
+        { answer: "O(nlog)", isCorrect: false },
+        { answer: "O(1)", isCorrect: false },
+      ],
     },
     {
       question: "What is the complexity of mergesort ?",
       options: [
-        { answer: 'O(n)', isCorrect: false},
-        { answer: 'O(logn)', isCorrect: false},
-        { answer: 'O(nlog)', isCorrect: true},
-        { answer: 'O(1)', isCorrect: false}
-      ]
+        { answer: "O(n)", isCorrect: false },
+        { answer: "O(logn)", isCorrect: false },
+        { answer: "O(nlog)", isCorrect: true },
+        { answer: "O(1)", isCorrect: false },
+      ],
     },
   ];
 
   function onNextClick() {
-    if(currentQuestion == quizQuestions.length - 1) return;
+    if (currentQuestion == quizQuestions.length - 1) return;
     setCurrentQuestion(currentQuestion + 1);
+    setOptionSelected(0);
   }
 
-  function checkAnswer(option) {
-    if(option.isCorrect) {
+  function checkAnswer(option, optno) {
+    if (optionSelected) return;
+    if (option.isCorrect) {
       console.log("Correct answer");
+      updateScore();
+      setOptionSelected(optno);
     } else {
       console.log("Wrong answer");
+      setOptionSelected(optno);
     }
   }
 
   return (
     <div className="card-wrapper">
+      <div>Score = {score}</div>
       <div className="app">
         <div className="question-section">
           <div className="question-count">
@@ -63,16 +72,30 @@ function App() {
           </div>
         </div>
         <div className="answer-section">
-            { quizQuestions[currentQuestion].options.map(option => <button onClick={() => checkAnswer(option)}>{option.answer}</button>) }
+          {quizQuestions[currentQuestion].options.map((option, idx) => (
+            <button
+              onClick={() => checkAnswer(option, idx + 1)}
+              key={idx}
+              className={
+                optionSelected
+                  ? optionSelected === idx + 1
+                    ? option.isCorrect
+                      ? "correct"
+                      : "incorrect"
+                    : ""
+                  : ""
+              }
+            >
+              {option.answer}
+            </button>
+          ))}
         </div>
       </div>
       {/* Button to show next question */}
       {/* onClick is a HOF, onNextClick is a callback function */}
       <button onClick={onNextClick}>Next</button>
     </div>
-    
   );
-  
 }
 
-export default App
+export default App;
